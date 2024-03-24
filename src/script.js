b9bd29b37043b6e91864c8e8a5105411d1c5e97d
BeckxBeck <rsheppard316@gmail.com>
2024-03-24 02:47:35 -0400

    fixed the search function; the input value now pulls weather data with axios

--- src/script.js
+++ src/script.js
@@ -15,12 +15,15 @@ function getDate() {
   let hour = now.getHours();
   let minute = now.getMinutes();
   
-  let currentDateInfo = document.querySelector("#current-date-info")
+  let currentDateInfo = document.querySelector("#current-date-info");
+  let meridiem = document.querySelector("#meridiem");
 
-  
   if(hour > 12) {
     hour = hour-12;  
-  }
+    meridiem = "pm"
+  } else {
+    meridiem = "am"
+  };
   
   // add Ante/Post Meridiem conditional
   
@@ -31,30 +34,40 @@ function getDate() {
     minute = minute;
   }
   
-  currentDateInfo.innerHTML = `${currentDay} ${hour}:${minute}`
-}
+  currentDateInfo.innerHTML = `${currentDay} ${hour}:${minute}${meridiem}`
+};
 
-setInterval(getDate, 1000)
+setInterval(getDate, 1000);
 
 
 
 
 // City search engine function *START HERE after reading comments 
+
+
+
 function searchCity(event) {
   event.preventDefault();
+  
+  let currentCityElement = document.querySelector(".current-city")
+  currentCityElement.innerHTML = searchInput.value
 
-  let currentCity = document.querySelector(".current-city")
-  currentCity.innerHTML = searchInput.value
+  function displayCityInfo(response) {
+    console.log(response.data)
+  }
+  let weatherUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=imperial`;
+  axios.get(weatherUrl).then(displayCityInfo) 
 }
+
+
+
 
 let apiKey = "333a6dab9tcf6097oe954a0dcb8f80ec";
 let searchInput = document.querySelector("#search-input")
-let weatherUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=imperial`;
 
 let searchForm = document.querySelector("#search-form")
 searchForm.addEventListener("submit", searchCity)
 
-// axios.get(weatherUrl).then(searchCity) 
 // gotta change the name of the function or modify it (nest functions? change exising function to submitForm and nest the actual searchCity func)
 
 /* Instructions
